3d9d85bbffd93197cf792957b2bbf236d7caf0bd
Wugawuga <dnr3655@naver.com>
2022-04-08 17:29:12 +0900

    추가 - 인프런 이진수 출력(재귀)

--- src/inflearn/Quiz11.java
+++ src/inflearn/Quiz11.java
@@ -1,2 +1,31 @@
-package inflearn;public class Quiz11 {
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 인프런 이진수 출력(재귀)
+public class Quiz11 {
+
+    public static void DFS(int N) {
+
+        if (N == 0) {
+            return;
+        } else {
+            DFS(N / 2);
+            System.out.print(N%2);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println(0);
+        } else {
+            DFS(N);
+        }
+    }
 }
