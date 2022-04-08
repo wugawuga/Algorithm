2866a491a1420919bcb42a249b37dedfbf384f06
Wugawuga <dnr3655@naver.com>
2022-04-08 10:40:42 +0900

    추가 - 인프런 숫자만 추출

--- src/inflearn/Quiz09.java
+++ src/inflearn/Quiz09.java
@@ -1,2 +1,40 @@
-package inflearn;public class Quiz09 {
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 인프런 숫자만 추출
+public class Quiz09 {
+
+    public static int solution(String str) {
+
+        int answer = 0;
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) {
+                answer = answer * 10 + (x-48);
+            }
+        }
+        return answer;
+    }
+
+    public static String solution1(String str) {
+
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) {
+                answer += x;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str)+" "+solution1(str));
+    }
 }
