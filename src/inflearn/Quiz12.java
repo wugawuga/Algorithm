2ec26a40788cefe39e0bc5dc30afddecd6044b08
Wugawuga <dnr3655@naver.com>
2022-04-08 17:29:29 +0900

    추가 - 인프런 팩토리얼

--- src/inflearn/Quiz12.java
+++ src/inflearn/Quiz12.java
@@ -1,2 +1,27 @@
-package inflearn;public class Quiz12 {
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 인프런 팩토리얼
+public class Quiz12 {
+
+    public static int DFS(int N) {
+
+        if (N == 1) {
+            return 1;
+        } else {
+            return N * DFS(N - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(DFS(N));
+    }
 }
