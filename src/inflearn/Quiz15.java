4b9faaf2d9e0d3d4dc2c63b5d8689be3f408ba39
Wugawuga <dnr3655@naver.com>
2022-04-09 12:35:55 +0900

    추가 - 인프런 문자거리

--- src/inflearn/Quiz15.java
+++ src/inflearn/Quiz15.java
@@ -1,2 +1,47 @@
-package inflearn;public class Quiz15 {
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 인프런 문자거리
+public class Quiz15 {
+
+    public static int[] solution(String s, char c) {
+
+        int[] answer=new int[s.length()];
+        int p=1000;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==c){
+                p=0;
+                answer[i]=p;
+            }
+            else{
+                p++;
+                answer[i]=p;
+            }
+        }
+        p=1000;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==c) p=0;
+            else{
+                p++;
+                answer[i]=Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        for (int i : solution(s, c)) {
+            System.out.print(i+" ");
+        }
+
+    }
 }
