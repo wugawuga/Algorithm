2a9b07602790cce1660bf57370755fac55bbc23f
Wugawuga <dnr3655@naver.com>
2022-04-09 12:36:37 +0900

    추가 - 인프런 암호(replace(), parseInt(String,2))

--- src/inflearn/Quiz17.java
+++ src/inflearn/Quiz17.java
@@ -1,2 +1,28 @@
-package inflearn;public class Quiz17 {
+package inflearn;
+
+import java.util.Scanner;
+
+// 인프런 암호(replace(), parseInt(String,2))
+public class Quiz17 {
+
+    public static String solution(int n, String s) {
+
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char)num;
+            s = s.substring(7);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String str = sc.next();
+
+        System.out.println(solution(N, str));
+    }
 }
