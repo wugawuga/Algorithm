366266787ea660b9dc17941499b9d848ec326d6a
Wugawuga <dnr3655@naver.com>
2022-04-09 12:36:17 +0900

    추가 - 인프런 문자열 압축

--- src/inflearn/Quiz16.java
+++ src/inflearn/Quiz16.java
@@ -1,2 +1,29 @@
-package inflearn;public class Quiz16 {
+package inflearn;
+
+import java.util.Scanner;
+
+// 인프런 문자열 압축
+public class Quiz16 {
+
+    public static String solution(String s){
+        String answer="";
+        s=s+" ";
+        int cnt=1;
+        for(int i=0; i<s.length()-1; i++){
+            if(s.charAt(i)==s.charAt(i+1)) cnt++;
+            else{
+                answer += s.charAt(i);
+                if(cnt>1) answer+=String.valueOf(cnt);
+                cnt=1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.println(solution(str));
+    }
 }
