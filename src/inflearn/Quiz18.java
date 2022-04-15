be1465e76cf422471254507995954b219e1f8a54
Wugawuga <dnr3655@naver.com>
2022-04-15 19:48:53 +0900

    추가 - 인프런 최대 길이 연속부분수열

---
+++ src/inflearn/Quiz18.java
@@ -0,0 +1,32 @@
+package inflearn;
+
+import java.util.Scanner;
+
+// 인프런 최대 길이 연속부분수열
+public class Quiz18 {
+
+    public static int solution(int n, int k, int[] arr){
+        int answer=0, cnt=0, lt=0;
+        for(int rt=0; rt<n; rt++){
+            if(arr[rt]==0) cnt++;
+            while(cnt>k){
+                if(arr[lt]==0) cnt--;
+                lt++;
+            }
+            answer=Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int k=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        System.out.print(solution(n, k, arr));
+    }
+}
