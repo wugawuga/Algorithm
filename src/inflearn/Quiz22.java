e5ca62a6938e68098cdab5db62bda03ff73ca1c5
Wugawuga <dnr3655@naver.com>
2022-04-20 21:20:54 +0900

    추가 - 인프런 모든 아나그램 찾기

---
+++ src/inflearn/Quiz22.java
@@ -0,0 +1,34 @@
+package inflearn;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+// 인프런 모든 아나그램 찾기
+public class Quiz22 {
+
+    public static int solution(String a, String b){
+        int answer=0;
+        HashMap<Character, Integer> am=new HashMap<>();
+        HashMap<Character, Integer> bm=new HashMap<>();
+        for(char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0)+1);
+        int L=b.length()-1;
+        for(int i=0; i<L; i++) am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0)+1);
+        int lt=0;
+        for(int rt=L; rt<a.length(); rt++){
+            am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0)+1);
+            if(am.equals(bm)) answer++;
+            am.put(a.charAt(lt), am.get(a.charAt(lt))-1);
+            if(am.get(a.charAt(lt))==0) am.remove(a.charAt(lt));
+            lt++;
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        String a=kb.next();
+        String b=kb.next();
+        System.out.print(solution(a, b));
+    }
+}
