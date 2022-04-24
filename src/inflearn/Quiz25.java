60b86052f86fc1c0e5181a0529e4e9896ba3f7aa
Wugawuga <dnr3655@naver.com>
2022-04-24 23:38:38 +0900

    추가 - 인프런 괄호문자제거

---
+++ src/inflearn/Quiz25.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 인프런 올바른 괄호
+public class Quiz25 {
+
+    public static String solution(String str){
+        String answer="";
+        Stack<Character> stack=new Stack<>();
+        for(char x : str.toCharArray()){
+            if(x==')'){
+                while(stack.pop()!='(');
+            }
+            else stack.push(x);
+        }
+        for(int i=0; i<stack.size(); i++) answer+=stack.get(i);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.println(solution(str));
+    }
+}
