aa3b29ce624dac6006a14486012fd2da4854ffb5
Wugawuga <dnr3655@naver.com>
2022-04-25 21:20:03 +0900

    추가 - 인프런 크레인인형뽑기

---
+++ src/inflearn/Quiz26.java
@@ -0,0 +1,42 @@
+package inflearn;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 인프런 크레인인형뽑기
+public class Quiz26 {
+
+    public static int solution(int[][] board, int[] moves){
+        int answer=0;
+        Stack<Integer> stack = new Stack<>();
+        for(int pos : moves){
+            for(int i=0; i<board.length; i++){
+                if(board[i][pos-1]!=0){
+                    int tmp=board[i][pos-1];
+                    board[i][pos-1]=0;
+                    if(!stack.isEmpty() && tmp==stack.peek()){
+                        answer+=2;
+                        stack.pop();
+                    }
+                    else stack.push(tmp);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[][] board=new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j]=kb.nextInt();
+            }
+        }
+        int m=kb.nextInt();
+        int[] moves=new int[m];
+        for(int i=0; i<m; i++) moves[i]=kb.nextInt();
+        System.out.println(solution(board, moves));
+    }
+}
