635445a79a860c10a4af08a03fb832e5e5039e6f
Wugawuga <dnr3655@naver.com>
2022-04-26 13:45:41 +0900

    추가 - 백준 2002

--- src/backJoon/Quiz26.java
+++ src/backJoon/Quiz26.java
@@ -1,2 +1,34 @@
-package backJoon;public class Quiz26 {
+package backJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+// https://www.acmicpc.net/problem/2002
+public class Quiz26 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> start = new HashMap<>();
+        int x = Integer.parseInt(br.readLine());
+        for (int i = 0; i < x; i++) {
+            start.put(br.readLine(), i);
+        }
+        String[] end = new String[x];
+        int cnt = 0;
+        for (int i = 0; i < x; i++) {
+            end[i] = br.readLine();
+        }
+        for (int i = 0; i < x; i++) {
+            for (int j = i + 1; j < x; j++) {
+                if (start.get(end[i]) > start.get(end[j])) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
