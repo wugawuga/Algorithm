5284501199b37ad2f34c65affb4e9595eef5f268
Wugawuga <dnr3655@naver.com>
2022-04-29 12:27:48 +0900

    추가 - 백준 10799

--- src/backJoon/Quiz33.java
+++ src/backJoon/Quiz33.java
@@ -1,2 +1,34 @@
-package backJoon;public class Quiz33 {
+package backJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//https://www.acmicpc.net/problem/10799
+public class Quiz33 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        Stack<Character> stk = new Stack<>();
+
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stk.push(c);
+            }else {
+                stk.pop();
+                if (s.charAt(i - 1) == '(') {
+                    cnt += stk.size();
+                }else
+                    cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
