6f412aa999b18dd8a07f43756fa5e6747300bf0e
Wugawuga <dnr3655@naver.com>
2022-05-02 11:11:14 +0900

    추가 - 인프런 응급실

---
+++ src/inflearn/Quiz29.java
@@ -0,0 +1,58 @@
+package inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// 인프런 응급실
+class Person {
+    
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
+public class Quiz29 {
+
+    public static int solution(int n, int m, int[] arr) {
+
+        int answer = 0;
+        Queue<Person> q = new LinkedList();
+        for (int i = 0; i < n; i++) {
+            q.offer(new Person(i, arr[i]));
+        }
+        while (!q.isEmpty()) {
+            Person tmp = q.poll();
+            for (Person x : q) {
+                if (x.priority > tmp.priority) {
+                    q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) {
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(n, m, arr));
+    }
+}
