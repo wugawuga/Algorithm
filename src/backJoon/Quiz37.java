3a1c6cb404de954199cc849de6107edb440eb369
Wugawuga <dnr3655@naver.com>
2022-05-04 12:48:29 +0900

    추가 - 프로그래머스 1차 캐시

--- src/backJoon/Quiz37.java
+++ src/backJoon/Quiz37.java
@@ -1,2 +1,46 @@
-package backJoon;public class Quiz37 {
+package backJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//https://programmers.co.kr/learn/courses/30/lessons/17680
+public class Quiz37 {
+
+    public static int solution(int cacheSize, String[] arr) {
+
+        int answer = 0;
+        Queue<String> cache = new LinkedList<>();
+
+        // 캐시 크기가 0이면 항상 캐시 miss이므로 모든 수행시간이 5초임
+        if (cacheSize == 0) {
+            return arr.length * 5;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            String city = arr[i];
+
+            if (!cache.contains(city)) { // 캐시 miss
+                answer = answer + 5;
+                if (cache.size() == cacheSize) {
+                    cache.poll();
+                }
+            } else { // 캐시 hit
+                answer = answer + 1;
+                cache.remove(city);
+            }
+            cache.add(city);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cache = Integer.parseInt(br.readLine());
+        String[] arr = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        System.out.println(solution(cache, arr));
+    }
 }
