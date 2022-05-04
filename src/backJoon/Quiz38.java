eec533071ce5a1fb9315ce321ed4be309d7a3c6a
Wugawuga <dnr3655@naver.com>
2022-05-04 12:48:55 +0900

    추가 - 백준 2309

--- src/backJoon/Quiz38.java
+++ src/backJoon/Quiz38.java
@@ -1,2 +1,36 @@
-package backJoon;public class Quiz38 {
+package backJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//https://www.acmicpc.net/problem/2309
+public class Quiz38 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k == i || k == j) {
+                            continue;
+                        }
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
 }
