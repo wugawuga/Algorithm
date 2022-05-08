6f5213a3fb7b8682f839f10b7e1960a98426ebb1
Wugawuga <dnr3655@naver.com>
2022-05-08 22:59:56 +0900

    추가 - 인프런 좌표정렬

---
+++ src/inflearn/Quiz33.java
@@ -0,0 +1,34 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// 인프런 좌표정렬
+class Point implements Comparable<Point>{
+    public int x, y;
+    Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    @Override
+    public int compareTo(Point o){
+        if(this.x==o.x) return this.y-o.y;
+        else return this.x-o.x;
+    }
+}
+
+class Quiz33 {
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        ArrayList<Point> arr=new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int x=kb.nextInt();
+            int y=kb.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        for(Point o : arr) System.out.println(o.x+" "+o.y);
+    }
+}
