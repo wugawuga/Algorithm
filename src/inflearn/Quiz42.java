2ee4f91ed238e1700502aef204a739a7aea83fb7
Wugawuga <dnr3655@naver.com>
2022-05-19 22:25:09 +0900

    추가 - 인프런 경로탐색(DFS)

---
+++ src/inflearn/Quiz42.java
@@ -0,0 +1,39 @@
+package inflearn;
+
+import java.util.Scanner;
+
+// 인프런 경로탐색(DFS)
+public class Quiz42 {
+    static int n, m, answer=0;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v){
+        if(v==n) answer++;
+        else{
+            for(int i=1; i<=n; i++){
+                if(graph[v][i]==1 && ch[i]==0){
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Quiz42 T = new Quiz42();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        graph=new int[n+1][n+1];
+        ch=new int[n+1];
+        for(int i=0; i<m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            graph[a][b]=1;
+        }
+        ch[1]=1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
